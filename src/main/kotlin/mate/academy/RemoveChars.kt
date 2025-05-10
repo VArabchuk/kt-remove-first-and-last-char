ab2b8142f061c65042a61a84a946d3ffa5b6af1c
VArabchuk <arabon3@gmail.com>
2025-05-10 20:37:20 +0300

    Initial commit

--- src/main/kotlin/mate/academy/RemoveChars.kt
+++ src/main/kotlin/mate/academy/RemoveChars.kt
@@ -1,6 +1,7 @@
 package mate.academy
 
+private const val INDEX = 1;
+
 fun removeChars(str: String): String {
-    // Implement this function
-    return ""
+    return str.substring(INDEX, str.length - INDEX)
 }
